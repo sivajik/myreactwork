ac3916ff62cf6889f89ae21c9de77cf3dc03e716
Sivaji Kondapalli <sivaji.kondapalli@ianwilliams.co.uk>
2025-02-28 14:09:12 +0000

    nesting react components

--- 01_inception/app.js
+++ 01_inception/app.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 //console.log(heading);
 
 // React Element
-const heading = (
+const TitleComponent = () => (
   <h1 className="head" tabIndex="1">
     Namaste React using JSX
   </h1>
@@ -14,9 +14,12 @@ const heading = (
 // React Componenet
 // 1. Class base components -> old way of writing code
 // 2. Functional components -> new way of writing code
+const number = 10000;
 
 const HeadingComponent = () => (
   <div id="container">
+    <TitleComponent />
+    <h2>{number}</h2>
     <h1 className="heading">Hello World Functional Componenet</h1>
   </div>
 );
