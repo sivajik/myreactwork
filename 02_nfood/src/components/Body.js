657d930bdfab811ddf7942f7c747a2052012c2b5
Sivaji Kondapalli <sivaji.kondapalli@ianwilliams.co.uk>
2025-03-07 21:34:05 +0000

    adding login/logout btn

--- 02_nfood/src/components/Body.js
+++ 02_nfood/src/components/Body.js
@@ -21,11 +21,9 @@ const Body = () => {
     );
   };
 
-  if (listOfRestaurants.length == 0) {
-    return <Shimmer />;
-  }
-
-  return (
+  return listOfRestaurants.length == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
--- 02_nfood/src/components/Header.js
+++ 02_nfood/src/components/Header.js
@@ -1,6 +1,17 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
+  const btnClicked = () => {
+    if (btnName == "Login") {
+      setBtnName("Logout");
+    } else {
+      setBtnName("Login");
+    }
+  };
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +23,9 @@ const Header = () => {
           <li>About us</li>
           <li>Contact us</li>
           <li>Cart</li>
+          <button className="login-btn" onClick={btnClicked}>
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
