9b4ecae702dff2bfe20deb4055830456cfff9c5a
Sivaji Kondapalli <sivaji.kondapalli@ianwilliams.co.uk>
2025-03-15 14:36:11 +0000

    enble lazy loading

--- 02_nfood/src/App.js
+++ 02_nfood/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
@@ -7,6 +7,16 @@ import Error from "./components/Error";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import RestaurantMenu from "./components/RestaurantMenu";
+//import Grocery from "./components/Grocery";
+
+// Chunking
+// Code Splitting
+// Dynamic Bundling
+// Lazy Loading
+// On Demand Loading
+// Dynamic importing
+
+const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
   return (
@@ -37,6 +47,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>still loading grocery</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restuarants/:resid",
--- 02_nfood/src/components/Header.js
+++ 02_nfood/src/components/Header.js
@@ -34,6 +34,9 @@ const Header = () => {
           <li>
             <Link to="/contact">Contact us</Link>
           </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
           <li>Cart</li>
           <button className="login-btn" onClick={btnClicked}>
             {btnName}
